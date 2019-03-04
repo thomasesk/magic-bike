857c0e0cd1ff370f24bd453d755ac96944bcee39
Thomas Eskenazi <thomas.eskenazi@hec.edu>
2019-03-04 16:31:57 +0100

    fix typos

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :bikes do
-    resources :bookings, only [:create]
+    resources :bookings, only: [:create]
   end
-  resources :bookings, only [:edit, :update, :index]
+  resources :bookings, only: [:edit, :update, :index]
   root to: 'pages#home'
 end
