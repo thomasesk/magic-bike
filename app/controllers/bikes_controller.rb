04e23e020d982a613be108d6bdb96f857fd1c989
Thomas Eskenazi <thomas.eskenazi@hec.edu>
2019-03-05 10:45:21 +0100

    Ajout de la méthode index, show, new et create au controller Bikes

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,14 +1,22 @@
 class BikesController < ApplicationController
   def index
+    @bikes = Bike.all
   end
 
   def show
+    @bike = Bike.find(params[:id])
   end
 
   def new
+    @bike = Bike.new
   end
 
   def create
+    @bike = Bike.build(set_params)
+    if @bike.save
+      redirect_to bike_path(@bike)
+    else
+      render :new
   end
 
   def edit
@@ -18,5 +26,11 @@ class BikesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_params
+    params.require(:bikes).permit(:name, :type, :price, :start_date, :end_date, :description, :address, :city, :zipcode, :country, :helmet, :locket, :rack, :photo)
   end
 end
