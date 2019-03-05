97408b6cf63bfee23b2f0adbdd56b5227f30d14e
Corentin Mathias <corentin.mathias@hec.edu>
2019-03-05 10:56:52 +0100

    controller for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,22 +1,54 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
   def index
+    @bookings = Booking.all
   end
 
   def show
   end
 
   def new
+    @bike = Bike.find(params[:bike_id])
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @bike = Bike.find(params[:bike_id])
+    @booking.bike = @bike
+    @user = current_user
+    @booking.user = @user
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @booking.update(booking_params)
+    redirect_to bookings_path
   end
 
   def destroy
+    # @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to root_path
   end
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :status, :pax)
+  end
+
 end
