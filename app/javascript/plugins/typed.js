09ad149780efc51276e684e2c9cb92c7571e57ba
Thomas Eskenazi <thomas.eskenazi@hec.edu>
2019-03-06 12:58:25 +0100

    Création de la map sur l'index

---
+++ app/javascript/plugins/typed.js
@@ -0,0 +1,14 @@
+import Typed from 'typed.js';
+
+const loadDynamicBannerText = () => {
+  const banner = document.getElementById('banner-typed-text');
+  if (banner) {
+    new Typed('#banner-typed-text', {
+      strings: ["Paris ^1000", "Londres ^1000", "New York ^1000", "Tokyo ^1000", "Melbourne ^1000"],
+      typeSpeed: 50,
+      loop: true
+    });
+  }
+};
+
+export { loadDynamicBannerText };
